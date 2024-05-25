72087b60ef03fcc28fdb090d44f8607fdc2b2b74
Malith Kuruppu <info.mkappworks@gmail.com>
2024-05-25 12:37:33 +0200

    Added lucia adapter and index

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,15 +1,13 @@
-import { DrizzlePostgreSQLAdapter } from "@lucia-auth/adapter-drizzle";
 import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
-import db from ".";
 
-const userTable = pgTable("user", {
+export const userTable = pgTable("user", {
   id: text("id").primaryKey(),
   username: text("username").notNull().unique(),
   hashedPassword: text("hashed_password"),
   googleId: text("google_id").unique(),
 });
 
-const sessionTable = pgTable("session", {
+export const sessionTable = pgTable("session", {
   id: text("id").primaryKey(),
   userId: text("user_id")
     .notNull()
@@ -19,7 +17,3 @@ const sessionTable = pgTable("session", {
     mode: "date",
   }).notNull(),
 });
-
-const adapter = new DrizzlePostgreSQLAdapter(db, sessionTable, userTable);
-
-export default adapter;

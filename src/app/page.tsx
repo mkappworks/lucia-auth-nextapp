72debf985e4511ee6f7ec5c79d09a193c89040ac
Malith Kuruppu <info.mkappworks@gmail.com>
2024-05-28 15:40:14 +0200

    chore: Refactor page.tsx to simplify user check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,7 @@ import { redirect } from "next/navigation";
 export default async function Home() {
   const { user } = await validateRequest();
 
-  if (!user) {
-    return redirect("/sign-in");
-  }
+  if (!user) return redirect("/sign-in");
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
